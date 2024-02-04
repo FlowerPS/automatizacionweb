20c0ed194a83e1c9dcf5ed2359c7fad4744a1586
Flor Paucar <bpaucarsilva@gmail.com>
2024-02-04 01:08:09 -0500

    delete

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -11,7 +11,7 @@ By Flor Paucar
 */
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features ="src/test/recursos/features/testNG.feature", glue= {"com.primerproyecto.pom.flor.web.stepdefinition"}, tags = ""
+@CucumberOptions(features ="src/test/recursos/features/HomeAmazon.feature", glue= {"com.primerproyecto.pom.flor.web.stepdefinition"}, tags = ""
         //plugin = {"pretty","html:target/cucumber-reports/cucumber-pretty","json:target/cucumber-reports/CucumberTestReport.json","rerun:target/cucumber-reports/rerun.txt"}
         )
 public class RunnerTest{
